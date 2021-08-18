511bc7e3ba238a5fed148a77fcc8896f0cedd266
kc emmanuel <fidekg123@gmail.com>
2021-08-19 00:19:23 +0100

    herokuCommit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
@@ -117,13 +118,17 @@ USE_L10N = True
 USE_TZ = True
 
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
